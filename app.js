b7ba865da8a2918b0e7d954ea73b8664ab77b404
Mason <masonlgoetz@gmail.com>
2015-04-13 17:29:01 -0600

    what the fuck is wrong with heroku?

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', __dirname + '/views');
 
 
 // stackoverflow suggestion for cross-domain
-// try this and this
+// try this and this | work heroku!
 app.all('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
