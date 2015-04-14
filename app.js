321dab55ddc733a81e85f2ef53e8d5e7dba5e094
Mason <masonlgoetz@gmail.com>
2015-04-14 16:38:03 -0600

    trying the CORS approach

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ app.set('views', __dirname + '/views');
 
 // stackoverflow suggestion for cross-domain
 // try this and this | work heroku!
-app.all('/', function(req, res, next) {
+/*app.all('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
+});*/
+
+// CORS suggestion
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // app.use(allowCrossDomain);
